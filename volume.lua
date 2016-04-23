3782b8e56f802dc31f94b18e3d2129a38f1858bb
Kevin Boxhoorn <kevinboxhoorn@yahoo.com>
2016-04-23 20:42:55 +1000

    Add icon to volume widget
    Add a dynamic icon to the volume widget. It changes according to the muted
    status of the Master Alsa track.

--- volume.lua
+++ volume.lua
@@ -1,31 +1,42 @@
 local wibox = require("wibox")
 local awful = require("awful")
 
-myvolume = wibox.widget.textbox()
-myvolume:set_align("right")
+local volumeicon = "/usr/share/awesome/themes/custom/icons/volume.png"
+local volumemuteicon = "/usr/share/awesome/themes/custom/icons/volumemute.png"
 
-function update_volume (widget)
+myvolume = wibox.layout.fixed.horizontal()
+
+myvolumeicon = wibox.widget.imagebox()
+myvolume:add(myvolumeicon)
+
+myvolumetext = wibox.widget.textbox()
+myvolumetext:set_align("right")
+myvolume:add(myvolumetext)
+
+function update_volume (widget, icon)
     local fd = io.popen("amixer sget Master")
     local status = fd:read("*all")
     fd:close()
 
     local volume = string.match(status, "(%d?%d?%d)%%")
-    volume = string.format("% 3d", volume)
+    volume = string.format("% 3d", volume) .. "%"
 
     status = string.match(status, "%[(o[^%]]*)%]")
 
     if string.find(status, "on", 1, true) then
         -- For the volume number percentage
-        volume = volume .. "%"
+        icon:set_image(volumeicon)
     else
         -- For displaying the mute status.
-        volume = volume .. "M"
+        icon:set_image(volumemuteicon)
     end
     widget:set_markup(volume)
 end
 
-update_volume(myvolume)
+update_volume(myvolumetext, myvolumeicon)
 
 mytimer = timer({ timeout = 0.2 })
-mytimer:connect_signal("timeout", function () update_volume(myvolume) end)
+mytimer:connect_signal("timeout", function ()
+		update_volume(myvolumetext, myvolumeicon)
+end)
 mytimer:start()
