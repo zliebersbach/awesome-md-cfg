0091bc5cec4a63f57f2d749aafe289248729a0ad
Kevin Boxhoorn <kevinboxhoorn@yahoo.com>
2016-04-24 20:31:00 +1000

    Complete temperature widget
    Properly parse output from sensors.
     - Check number of cores.
     - Get the temperature of each core.
     - Choose the hottest core temperature to display on the widget.
    Add icon and margin of 4.

--- rc.lua
+++ rc.lua
@@ -225,7 +225,7 @@ for s = 1, screen.count() do
 	-- Widgets that are aligned to the right
 	local right_layout = wibox.layout.fixed.horizontal()
 	if s == 1 then right_layout:add(wibox.widget.systray()) end
-	--right_layout:add(mytemp)
+	right_layout:add(mytemp)
 	right_layout:add(myvolume)
 	right_layout:add(mytextclock)
 	right_layout:add(mylayoutbox[s])
--- temp.lua
+++ temp.lua
@@ -1,19 +1,37 @@
 local wibox = require("wibox")
 local awful = require("awful")
 
-mytemp = wibox.widget.textbox()
-mytemp:set_align("right")
+mytemp = wibox.layout.margin()
+mytemp:set_left(4)
+mytemp:set_right(4)
+
+mytemplayout = wibox.layout.fixed.horizontal()
+mytemp:set_widget(mytemplayout)
+
+mytempicon = wibox.widget.imagebox()
+mytempicon:set_image("/usr/share/awesome/themes/custom/icons/cpu.png")
+mytemplayout:add(mytempicon)
+
+mytemptext = wibox.widget.textbox()
+mytemptext:set_align("right")
+mytemplayout:add(mytemptext)
 
 function update_temp (widget)
 	local fd = io.popen("sensors -u coretemp-isa-0000")
 	local status = fd:read("*all")
 	fd:close()
 
-	widget:set_markup(status)
+	local temps = {}
+	local _, corecount = string.gsub(status, "Core %d", "%1")
+	for core=0, corecount, 1 do
+		temps[core] = string.match(status, "temp%d_input: (%d+%.%d+)")
+	end
+	
+	widget:set_markup(math.max(unpack(temps)) .. " °C")
 end
 
-update_temp(mytemp)
+update_temp(mytemptext)
 
 mytimer = timer({ timeout = 10.0 })
-mytimer:connect_signal("timeout", function () update_temp(mytemp) end)
+mytimer:connect_signal("timeout", function () update_temp(mytemptext) end)
 mytimer:start()
