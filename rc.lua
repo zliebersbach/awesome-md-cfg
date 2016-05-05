6d94a0242babe1a2663ba91baa83bfa84c33cad6
Kevin Boxhoorn <kevinboxhoorn@yahoo.com>
2016-05-05 15:14:34 +1000

    Change Chromium WM class

--- rc.lua
+++ rc.lua
@@ -431,7 +431,7 @@ awful.rules.rules = {
 		  tag = tags[1][2]
 	} },
 	-- Open Chromium on second monitor
-	{ rule = { class = "chromium-browser" }, properties = {
+	{ rule = { class = "chromium" }, properties = {
 		  tag = tags[2][1]
 	} },
 	{ rule = {
