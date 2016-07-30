124999f573454db170e43cbf74396b3eca408dfa
Kevin Boxhoorn <kevinboxhoorn@yahoo.com>
2016-07-30 10:05:42 +1000

    Change wallpaper

--- themes/material-dark/theme.lua
+++ themes/material-dark/theme.lua
@@ -69,7 +69,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/mat
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/material-dark/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/material-dark/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/usr/share/awesome/themes/material-dark/wallpapers/wallpaper-forest-treetops.jpg"
+theme.wallpaper = "/usr/share/awesome/themes/material-dark/wallpapers/wallpaper.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/material-dark/layouts/fairhw.png"
@@ -88,7 +88,7 @@ theme.layout_dwindle = "/usr/share/awesome/themes/material-dark/layouts/dwindlew
 theme.menu_icon = "/usr/share/awesome/themes/material-dark/icons/menu.png"
 theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
--- Define the icon theme for application icons. If not set then the icons 
+-- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
 theme.icon_theme = "Adwaita"
 
